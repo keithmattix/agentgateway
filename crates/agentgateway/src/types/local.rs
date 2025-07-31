f2aaaf043a49ae4feac97132f6c85e6485d326ff
Keith Mattix II <keithmattix@microsoft.com>
2025-07-31 23:32:55 +0000

    feat: Introduce stateful_mode field for McpBackend
    Fixes #221
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- crates/agentgateway/src/types/local.rs
+++ crates/agentgateway/src/types/local.rs
@@ -242,7 +242,7 @@ impl LocalBackend {
 					};
 					targets.push(Arc::new(t));
 				}
-				let m = McpBackend { targets };
+				let m = McpBackend { targets, stateful: tgt.stateful };
 				backends.push(Backend::MCP(name, m));
 				(backends, policies)
 			},
@@ -253,8 +253,9 @@ impl LocalBackend {
 }
 
 #[apply(schema!)]
-pub struct LocalMcpBackend {
+pub struct LocalMcpBackend { // keithmattix: Is this used??
 	pub targets: Vec<Arc<LocalMcpTarget>>,
+	pub stateful: bool
 }
 
 #[apply(schema!)]
