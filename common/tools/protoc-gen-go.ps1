f692f9e9cf0f66a0254a6f96cb8ea10dd8054e57
keithmattix <keithmattix@microsoft.com>
2025-08-18 14:15:38 -0500

    Fixups
    Signed-off-by: keithmattix <keithmattix@microsoft.com>

--- common/tools/protoc-gen-go.ps1
+++ common/tools/protoc-gen-go.ps1
@@ -4,5 +4,7 @@
 $WD = Split-Path -Parent $MyInvocation.MyCommand.Path
 $WD = Resolve-Path $WD
 
+echo $WD
+
 # Execute the protoc-gen-go tool with the specified module file
 & go tool -modfile="$WD/go.mod" "google.golang.org/protobuf/cmd/protoc-gen-go" $args
