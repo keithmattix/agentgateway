8db7d01dd81e0df9db5cdf8b5d218f70fc3839b0
Keith Mattix II <keithmattix2@gmail.com>
2025-08-18 21:20:51 -0500

    Iterate on local branch to make GH Actions work
    Fixup scripts and skip the keycloak test on Windows

--- crates/agentgateway/src/cel/tests.rs
+++ crates/agentgateway/src/cel/tests.rs
@@ -73,6 +73,7 @@ fn bench_native(b: Bencher) {
 }
 
 #[divan::bench]
+#[cfg(target_family = "unix")]
 fn bench_native_map(b: Bencher) {
 	let req = ::http::Request::builder()
 		.method(Method::GET)
@@ -104,6 +105,7 @@ macro_rules! function {
 	}};
 }
 
+#[cfg(target_family = "unix")]
 fn with_profiling(name: &str, f: impl FnOnce()) {
 	use pprof::protos::Message;
 	let guard = pprof::ProfilerGuardBuilder::default()
@@ -125,6 +127,7 @@ fn with_profiling(name: &str, f: impl FnOnce()) {
 }
 
 #[divan::bench]
+#[cfg(target_family = "unix")]
 fn bench_lookup(b: Bencher) {
 	let expr = Arc::new(Expression::new(r#"request.method"#).unwrap());
 	let ctx = root_context();
@@ -168,6 +171,7 @@ fn bench_with_response(b: Bencher) {
 }
 
 #[divan::bench]
+#[cfg(target_family = "unix")]
 fn benchmark_register_build(b: Bencher) {
 	let expr = Arc::new(Expression::new(r#"1 + 2 == 3"#).unwrap());
 	with_profiling("full", || {
