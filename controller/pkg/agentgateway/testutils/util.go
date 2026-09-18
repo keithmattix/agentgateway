e52ec6342e80f13b72c8dd10b1caac2ac87837f3
John Howard <john.howard@solo.io>
2026-09-18 14:18:13 -0700

    controller: share common Istio scheme (#3559)
    This avoids ~500kb copies of the same thing in a few places
    
    Signed-off-by: John Howard <john.howard@solo.io>
    
    Signed-off-by: John Howard <john.howard@solo.io>

--- controller/pkg/agentgateway/testutils/util.go
+++ controller/pkg/agentgateway/testutils/util.go
@@ -18,7 +18,6 @@ import (
 	"istio.io/istio/pkg/test/util/file"
 	corev1 "k8s.io/api/core/v1"
 	discovery "k8s.io/api/discovery/v1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	inf "sigs.k8s.io/gateway-api-inference-extension/api/v1"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 	gwv1b1 "sigs.k8s.io/gateway-api/apis/v1beta1"
@@ -35,7 +34,6 @@ import (
 	"github.com/agentgateway/agentgateway/controller/pkg/apiclient/fake"
 	"github.com/agentgateway/agentgateway/controller/pkg/controller"
 	"github.com/agentgateway/agentgateway/controller/pkg/pluginsdk/krtutil"
-	"github.com/agentgateway/agentgateway/controller/pkg/schemes"
 	"github.com/agentgateway/agentgateway/controller/pkg/syncer"
 	"github.com/agentgateway/agentgateway/controller/pkg/syncer/status"
 	"github.com/agentgateway/agentgateway/controller/pkg/wellknown"
@@ -80,11 +78,6 @@ func isSpace(r rune) bool {
 		}
 	}
 	return false
-}
-
-func init() {
-	// Add our types to Istio since we are using their library
-	utilruntime.Must(schemes.AddToScheme(kube.IstioScheme))
 }
 
 func GetTestResource[T any](t *testing.T, collection krt.Collection[T]) T {
